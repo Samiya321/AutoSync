dc4ef72a239d723381097ab7cd3b416eac34ee58
elzari <86085151+elzari@users.noreply.github.com>
2022-09-01 07:21:08 +0200

    Add files via upload

---
+++ Tivify.js
@@ -0,0 +1,168 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+{
+  "numMaxSessions" : 2,
+  "pinEnabled" : true,
+  "province" : "Badajoz",
+  "status" : "activo",
+  "plan" : {
+    "id" : "6153152fac8b1b0021fc98bf",
+    "allowedSessions" : 2,
+    "isFree" : false,
+    "anonymous" : false,
+    "allowedDevices" : 5,
+    "advertisement" : {
+      "enabled" : false,
+      "adsProvider" : {
+        "endpoint" : "ctv33256up",
+        "code" : "tappx"
+      },
+      "sections" : {
+        "vastTags" : {
+          "channelU7d" : {
+            "enabled" : false
+          },
+          "seriesU7d" : {
+            "enabled" : false
+          },
+          "deadMan" : {
+            "enabled" : false
+          },
+          "channels" : {
+            "enabled" : false
+          },
+          "dial" : {
+            "enabled" : false
+          },
+          "cineU7d" : {
+            "enabled" : false
+          },
+          "myRecordings" : {
+            "enabled" : false
+          },
+          "guideTv" : {
+            "enabled" : false
+          },
+          "programsU7d" : {
+            "enabled" : false
+          },
+          "startApp" : {
+            "enabled" : false
+          },
+          "guidePlatforms" : {
+            "enabled" : false
+          }
+        },
+        "adsWindow" : {
+          "minDuration" : 10,
+          "maxDuration" : 120
+        },
+        "impressions" : {
+          "periodicity" : "day"
+        }
+      }
+    },
+    "powerSaving" : true,
+    "code" : "TPREMIUM",
+    "badge" : "/plans/6153152fac8b1b0021fc98bfbadge.png",
+    "colorHex" : "C9C9C9",
+    "upgradable" : true,
+    "name" : "Tivify Premium",
+    "features" : {
+      "id" : "6149dbea90dfa50022740122",
+      "startOver" : true,
+      "availableRecordingsDays" : 90,
+      "recordHours" : 350,
+      "iGuideTV" : true,
+      "code" : "tivify_premium_func",
+      "channelOrdering" : true,
+      "lastShows" : true,
+      "allowRecord" : true,
+      "chromecast" : true,
+      "name" : "Funcionalidades Tivify Premium",
+      "timeShift" : true
+    }
+  },
+  "lang" : "es",
+  "agreementCond" : true,
+  "notifications" : {
+    "email" : true,
+    "push" : true,
+    "recommendations" : true,
+    "newsLetter" : true,
+    "deactivateAllNotifications" : false,
+    "news" : true
+  },
+  "parentalPin" : 1111,
+  "_id" : "63103e63c0d8700023019e5e",
+  "changePassword" : false,
+  "numMaxDevices" : 5,
+  "city" : "Badajoz",
+  "geoblocked" : true,
+  "name" : "Federico",
+  "androidTVVersion" : [
+
+  ],
+  "recommendations" : false,
+  "roles" : [
+
+  ],
+  "token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMTAzZTYzYzBkODcwMDAyMzAxOWU1ZSIsInBsYXRmb3JtIjoiSVBIT05FIiwiaWREZXZpY2UiOiI2MDY2NEU0Mi0xOUQ5LTREQjctQjQ1Qy1DMjdERDU5MjNBNDgiLCJ2ZXJzaW9uIjoiMi4yMi4wIiwibW9kZWwiOiJpUGhvbmUxMlByb01heCIsImlhdCI6MTY2MjAwOTEzOSwiZXhwIjoxNjY5Nzg1MTM5fQ.UGbZ9PDUWZ_T098Hv54B6GY9QreTzARKI42DErDOmuU",
+  "email" : "fedemf38@icloud.com",
+  "carrier" : {
+    "id" : "5ea94948bf8cbb0072c89fa6",
+    "logoBlanco" : "https://media.tvup.cloud/carriers/5ea94948bf8cbb0072c89fa6logoBlanco.png?t=1661788861",
+    "logoServicio" : "https://media.tvup.cloud/carriers/5ea94948bf8cbb0072c89fa6logoServicio.png?t=1661788861",
+    "freemiumFilter" : true,
+    "code" : "Tivify B2C",
+    "usesTeltoo" : false,
+    "isHotel" : false,
+    "hotelLanguages" : [
+
+    ],
+    "logo" : "https://media.tvup.cloud/carriers/5ea94948bf8cbb0072c89fa6logo.png?t=1661788861",
+    "lang" : "es",
+    "appBackgroud" : {
+      "id" : "6182a26cf6a5dc00218c5211",
+      "backgroundImgCard" : "https://media.tvup.cloud/appBackgrounds/001-BACKGROUND-MINIFICHA.png",
+      "backgroundImgCardMask" : "https://media.tvup.cloud/appBackgrounds/002-MASK PHOTO-MINIFICHA.png",
+      "name" : "Tivify 2022",
+      "backgroundImgMask" : "https://media.tvup.cloud/appBackgrounds/001-BACKGROUND-MINIFICHA.png",
+      "backgroundImg" : "https://media.tvup.cloud/appBackgrounds/001.png"
+    },
+    "service" : "TivifyD2C",
+    "tokenExpires" : 7776000,
+    "name" : "Tivify B2C",
+    "isP2P" : true
+  },
+  "deleteCarrier" : false,
+  "trial" : false,
+  "currentPlanSubscription" : {
+    "paymentMethod" : "paytpv",
+    "planId" : "6153152fac8b1b0021fc98bf",
+    "periodicity" : 365,
+    "paymentDate" : "2022-09-01T05:09:42.000Z",
+    "createdByProductCheckout" : false,
+    "orderId" : "1037745",
+    "beginDate" : "2022-09-01T05:08:51.000Z",
+    "nextRenewal" : "2023-10-01T05:08:51.000Z",
+    "plan" : "TPREMIUM",
+    "firstPaymentDate" : "2022-09-01T05:09:34.000Z",
+    "status" : "activo",
+    "paymentStatus" : "activo"
+  },
+  "channels" : [
+
+  ],
+  "isAnonymous" : false,
+  "recordTime" : 0,
+  "surname" : "Muñoz Fernández",
+  "extraPlans" : [
+
+  ],
+  "postcode" : "06005",
+  "numMaxSTB" : 5,
+  "username" : "fedemf38@icloud.com"
+}
+$done({body: JSON.stringify(obj)});
