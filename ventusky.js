e6849cd49d624cc4bac7a542dbfd3205dba428ea
elzari <86085151+elzari@users.noreply.github.com>
2022-09-02 04:30:31 +0200

    Add files via upload

---
+++ ventusky.js
@@ -0,0 +1,11 @@
+var obj = JSON.parse($response.body);
+
+obj =
+
+{
+  "expire_timestamp" : null,
+  "timestamp" : 1662085538,
+  "time" : "2022-09-02 02:25:38",
+  "version" : "premium"
+}
+$done({body: JSON.stringify(obj)});
