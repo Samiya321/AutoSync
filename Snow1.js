c7ec02b32ee97df93c0e037b6c56207bb2fad688
elzari <86085151+elzari@users.noreply.github.com>
2022-09-11 07:52:30 +0200

    Add files via upload

---
+++ Snow1.js
@@ -0,0 +1,17 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "result": {
+    "activated": true,
+    "products": [{
+      "productId": "com.campmobile.snow.subscribe.onemonth",
+      "startDate":  4092787996000,
+      "expireDate": 1665465731000,
+      "managed": false,
+      "status": "ACTIVE"
+    }]
+  }
+}
+$done({body: JSON.stringify(obj)});
