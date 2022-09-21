5c0b775fd08e323962190a63f3185f321a353f88
elzari <86085151+elzari@users.noreply.github.com>
2022-09-21 05:08:23 +0200

    Add files via upload

---
+++ Airbrush_1.js
@@ -0,0 +1,16 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "code": 0,
+  "message": "success",
+  "data": {
+    "taskId": 0,
+    "ABTestCode": "",
+    "distributeTime": 0,
+    "validTerm": 0,
+    "userPopupStatus": 0
+  }
+}
+$done({body: JSON.stringify(obj)});
