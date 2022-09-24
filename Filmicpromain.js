20059c73d185be46de37f800d82b03cb63c023e4
elzari <86085151+elzari@users.noreply.github.com>
2022-09-24 02:38:08 +0200

    Add files via upload

---
+++ Filmicpromain.js
@@ -0,0 +1,127 @@
+var obj = JSON.parse($response.body);
+obj =
+
+{
+  "force_updater": {
+    "suggested_build_version": "0.0.0",
+    "suggested_update_frequency": 0.0,
+    "action": "do_nothing"
+  },
+  "legal_notifications": {
+    "to_notify_privacy_policy": false,
+    "to_notify_terms_of_service": false
+  },
+  "me": {
+    "active_bundle_subscriptions": [],
+    "active_subscriptions_ids": ["com.cinegenix.filmic.pro.1w_t6_1w_bis"],
+    "non_consumables_ids": [],
+    "is_spooner": false,
+    "privacy_notice": {
+      "last_acknowledged_version": "1.0.0",
+      "is_at_least_16": "unknown",
+      "required_consents": []
+    },
+    "terms_of_service": {
+      "last_accepted_version": "1.1.0"
+    },
+    "unique_id": "62A5DF84-FA73-41B7-A171-F82656CD0A44"
+  },
+  "products": {
+    "consumables": [],
+    "non_consumables": [{
+      "product_id": "CinematographerKit",
+      "status": "APPROVED"
+    }, {
+      "product_id": "FilterPackAdamant",
+      "status": "APPROVED"
+    }, {
+      "product_id": "FilterPackCharcoal",
+      "status": "APPROVED"
+    }, {
+      "product_id": "FilterPackInfrared",
+      "status": "APPROVED"
+    }, {
+      "product_id": "FilterPackNovus",
+      "status": "APPROVED"
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.filmic_standard_t15",
+      "status": "APPROVED"
+    }],
+    "subscriptions": [{
+      "product_id": "com.cinegenix.filmic.pro.1w_t6_1w",
+      "status": "APPROVED",
+      "features": []
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.1w_t10_1w",
+      "status": "APPROVED",
+      "features": []
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.1w_t6",
+      "status": "APPROVED",
+      "features": []
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.1w_t10",
+      "status": "APPROVED",
+      "features": []
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.1y_t80_1w",
+      "status": "APPROVED",
+      "features": []
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.1w_t6_bis",
+      "status": "APPROVED",
+      "features": []
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.1y_t80_1w_bis",
+      "status": "APPROVED",
+      "features": []
+    }, {
+      "product_id": "com.cinegenix.filmic.pro.1w_t6_1w_bis",
+      "status": "APPROVED",
+      "features": []
+    }]
+  },
+  "settings": {
+    "__experiments__": {
+      "hard_monetization": 1
+    },
+    "__identity__": {
+      "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6ImY1YWVmMWQ0ODg2YjQwZDY4MTIzZTk5OWQ4YWY5ZDljIn0.eyJwYXlsb2FkIjp7ImJhY2tlbmRfaWQiOiI2MkE1REY4NC1GQTczLTQxQjctQTE3MS1GODI2NTZDRDBBNDQiLCJ1c2VyX2lkIjoiQkNGNUExNEYtOTI1OC00QTIwLTg1NTMtNTU1RTQ0NTk1RjFDIiwiaXNfZnJlZSI6ZmFsc2UsImlzX3Nwb29uZXIiOmZhbHNlLCJpc19zdWJzY3JpYmVkIjp0cnVlLCJjdXJyZW50X2luc3RhbGxhdGlvbl90aW1lIjoiMjAyMi0wOC0yNVQxMjozNTo1MyswMDowMCIsImZpcnN0X2luc3RhbGxhdGlvbl90aW1lIjoiMjAyMi0wOC0yNVQxMjozNTo1MyswMDowMCIsInByaXZhY3lfcG9saWN5Ijp7Imxhc3RfYWNrbm93bGVkZ2VkX3ZlcnNpb24iOiIxLjAuMCJ9LCJzZXR0aW5ncyI6e319LCJpc3MiOiJvcmFjbGUyIiwiaWF0IjoxNjYzOTc4NTI5LCJuYmYiOjE2NjM5Nzg1MjksImV4cCI6MTY2NDA2NDkyOX0.Y5Dd14bW969EGpu4wZw_ZEy3qyD1O5Qa5v7vUsFBEVE",
+      "expiration": "2025-09-25T00:15:29.086188+00:00"
+    },
+    "__is_baseline__": false,
+    "__is_free__": false,
+    "__is_testflight__": false,
+    "__min_required_build_version__": "0.0.0",
+    "__privacy_notice_url__": "https://support.bendingspoons.com/privacy.html?app=436577167&version=1.0.0",
+    "__privacy_notice_version__": "1.0.0",
+    "__privacy_policy_url__": "https://support.bendingspoons.com/privacy.html?app=436577167&version=1.0.0",
+    "__privacy_policy_version__": "1.0.0",
+    "__terms_of_service_effective_date__": "2022-07-21T12:00:00+00:00",
+    "__terms_of_service_url__": "https://support.bendingspoons.com/tos.html?app=436577167&version=1.1.0",
+    "__terms_of_service_version__": "1.1.0",
+    "force_monetization_behaviour": "disabled",
+    "review_skip_survey": true,
+    "idfa_tracking_enabled": false,
+    "checkbox_subscription_id": "com.cinegenix.filmic.pro.1w_t6_1w",
+    "__encryption_public_key__": "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6WMNkl4avAdHhYGc20hq\n8DcVtWanEtvuzM7g80Pe8Tv+eyrGyO1+XrgOUu7FRW2TFAl1q1r+3Uls0Y4Xyevi\n1F52DlSrKUJ84fpHIlhMhG8fGKTtAEOZ/CeSj4eUIjGqZe79/BAsEs1A5TZEKdlw\n24oeuMklhCeJ09Im/cU5FQJ18f6Ew8Dqmp7lLKsXoUA8hhBAPbcN7cb4cX6pvNWC\n+alZ9yX2sm4gs3cqjHPsLIEptJkaR469ZcSxqbBNtduS+DMRyhVD+M9ekjdv6aaE\nmRuFumWRtLtGvL9QfyOvTQPsp4eI/O4iZUH7A/W9L67WiYWDPhyknGd7fjIaEedS\n5wIDAQAB\n-----END PUBLIC KEY-----",
+    "soft_trigger_factor": 0,
+    "subscription_paywall_type": "comparison",
+    "hard_trigger_factor": 1,
+    "comparison_subscription_id": "com.cinegenix.filmic.pro.1y_t80_1w",
+    "monetization_type": "subscription",
+    "min_time_between_requests": 180,
+    "comparison_secondary_subscription_id": "com.cinegenix.filmic.pro.1y_t80_1w_bis",
+    "max_requests_per_version": 5,
+    "__encryption_algorithm__": "RSA2048PKCS1",
+    "comparison_primary_subscription_id": "com.cinegenix.filmic.pro.1w_t6_1w_bis",
+    "keep_current_version_survey_link": "https://rc1userl9j3bfjcc7ck2.fra1.qualtrics.com/jfe/form/SV_297ajs1gCVrTk6a",
+    "review_show_native_prompt_chance": 0.5,
+    "full_app_lifetime_purchase_id": "com.cinegenix.filmic.pro.filmic_standard_t15",
+    "sign_up_survey_link": "https://rc1userl9j3bfjcc7ck2.fra1.qualtrics.com/jfe/form/SV_etvHY9g71ip6lZI",
+    "main_subscription_id": "com.cinegenix.filmic.pro.1w_t6",
+    "show_survey": false,
+    "__encryption_key_id__": "53bfd4440016c"
+  }
+}
+$done({body: JSON.stringify(obj)});
