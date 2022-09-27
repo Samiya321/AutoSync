051025b1380abef5f59a70c71300368c92550f10
elzari <86085151+elzari@users.noreply.github.com>
2022-09-27 02:15:48 +0200

    Add files via upload

---
+++ MusixAcademypoints.js
@@ -0,0 +1,55 @@
+
+
+{
+  "message": {
+    "header": {
+      "status_code": 200,
+      "execute_time": 0.0050170421600342
+    },
+    "body": {
+      "user": {
+        "uaid": "3vUCANZc26MXS_mwcjL4WiETnyuJ5HICXzLRAtPll6b3iGoXMGbSfRUxg4ceLBO6ONWRMc3xeRaO1ib6DgAocIMeRW60LypKVWIFYrWarxveg-pbL7s7BE0cEaURjerb66AeqVeqzlyXb2U7JIekeARuXs2c6DuQSdbBGntAAbMAKA4J723j_xvkplk",
+        "is_mine": 1,
+        "user_name": "Federico Muñoz Fernández",
+        "user_profile_photo": "https:\/\/www.gravatar.com\/avatar\/f978a0797c12ad709a3042e46a7cf80e?r=g&d=https%3A%2F%2Fstatic.musixmatch.com%2Fuser%2Fuser_empty.png",
+        "has_private_profile": 0,
+        "score": 65,
+        "position": 0,
+        "weekly_score": 0,
+        "level": "standard",
+        "key": "7108fef2614bf22b636692cb6342a2812cc83cb947dea7957f550490350cf177",
+        "rank_level": 3,
+        "points_to_next_level": 10,
+        "ratio_to_next_level": 0,
+        "rank_name": "newbie",
+        "next_rank_name": "insider",
+        "ratio_to_next_rank": 60,
+        "rank_color": "54A1FF",
+        "rank_colors": {
+          "rank_color_10": "DAEBFF",
+          "rank_color_50": "84BFFF",
+          "rank_color_100": "54A1FF",
+          "rank_color_200": "3E79D0"
+        },
+        "rank_image_url": "https:\/\/static.musixmatch.com\/badges\/rank_icons\/newbie.png",
+        "next_rank_color": "46D5B5",
+        "next_rank_colors": {
+          "rank_color_10": "DEFFF8",
+          "rank_color_50": "96E8D6",
+          "rank_color_100": "46D5B5",
+          "rank_color_200": "37B297"
+        },
+        "next_rank_image_url": "https:\/\/static.musixmatch.com\/badges\/rank_icons\/insider.png",
+        "counters": {
+          "evaluation_academy_test": 1,
+          "lyrics_favourite_added": 20
+        },
+        "artist_manager": 0,
+        "academy_completed": true,
+        "profile": {
+          "progress": 40
+        }
+      }
+    }
+  }
+}
