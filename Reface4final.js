0587c6433e3932a84e94c40d015d30df4c5e38aa
elzari <86085151+elzari@users.noreply.github.com>
2022-09-29 05:39:10 +0200

    Add files via upload

---
+++ Reface4final.js
@@ -0,0 +1,46 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "request_date": "2022-09-29T03:32:43Z",
+  "request_date_ms": 1664422363566,
+  "subscriber": {
+    "entitlements": {},
+    "first_seen": "2022-04-14T21:54:46Z",
+    "last_seen": "2022-09-29T02:44:44Z",
+    "management_url": null,
+    "non_subscriptions": {},
+    "original_app_user_id": "f2e303ff-8e22-48c1-b1c0-632a97b53d93",
+    "original_application_version": "2010",
+    "original_purchase_date": "2021-05-07T08:17:44Z",
+    "other_purchases": {},
+    "subscriptions": {
+      "com.neocortext.doublicatapp.subscription.bro.weekly": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2023-09-29T01:26:58Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2022-07-13T20:33:55Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "normal",
+        "purchase_date": "2022-09-22T01:26:58Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
+      },
+      "com.neocortext.doublicatapp.subscription.promos.annual.50off": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2023-07-13T06:16:18Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2022-07-10T06:16:19Z",
+        "ownership_type": "PURCHASED",
+        "period_type": "trial",
+        "purchase_date": "2022-07-10T06:16:18Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": null
+      }
+    }
+  }
+}
+$done({body: JSON.stringify(obj)});
