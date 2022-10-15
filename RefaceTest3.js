a095fac3bddcfe7bd4e9caca66adaec71368cce1
elzari <86085151+elzari@users.noreply.github.com>
2022-10-15 06:28:32 +0200

    Add files via upload

---
+++ RefaceTest3.js
@@ -0,0 +1,16 @@
+const res = $response;
+const req = $request;
+
+if (res.body && req.method == "PUT") {
+  $done({
+    body: JSON.stringify({
+      "app": "REFACE",
+      "isBro": "true",
+      "hasWatermark": "false"
+    }),
+    status: sta,
+    headers: res.headers
+  });
+} else {
+  $done({});
+}
