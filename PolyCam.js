5c74c208cfd062d3614a25d14131cce1b4934c99
elzari <86085151+elzari@users.noreply.github.com>
2022-10-17 06:12:23 +0200

    Add files via upload

---
+++ PolyCam.js
@@ -0,0 +1,11 @@
+var obj = JSON.parse($response.body);
+
+obj = {
+
+  "iap-300001249221451" : {
+    "tier" : "pro",
+    "expiresAt" : 4095893124000,
+    "id" : "iap-300001249221451",
+    "type" : "iap"
+  }
+}
