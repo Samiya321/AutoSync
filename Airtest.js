b138a033c953b4c8c5ae468aeef30e53928827a0
elzari <86085151+elzari@users.noreply.github.com>
2022-10-23 06:49:33 +0200

    Add files via upload

---
+++ Airtest.js
@@ -0,0 +1,476 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "code" : 0,
+  "data" : [
+    {
+      "deeplink" : "https://www.instagram.com/airbrush_latam/guide/c-mo-crear-la-edici-n-perfecta-de-halloween/17927023619599167/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000443",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1666310400,
+          1667264399
+        ],
+        "timezone" : {
+          "id" : "user_timezone",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "¡Crea ediciones increíbles con las herramientas de Halloween!",
+        "text" : "¡Crea ediciones increíbles con las herramientas de Halloween!"
+      },
+      "ended_at" : 1667264399,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/d980a2308548e76de609681b1a9cf4e8.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 363,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/las-herramientas-perfectas-para-crear-contenido-de-halloween-para-tu-marca/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000440",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1665792000,
+          1667264399
+        ],
+        "timezone" : {
+          "id" : "user_timezone",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "¡Crea contenido de Halloween para tu marca!",
+        "text" : "¡Crea contenido de Halloween para tu marca!"
+      },
+      "ended_at" : 1667264399,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/c4445a2ccbe3e6f89f1152a25ecc58af.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 348,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/5-formas-de-usar-fondos-que-no-conocias/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000399",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1660867200,
+          1667264399
+        ],
+        "timezone" : {
+          "id" : "user_timezone",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "La herramienta Fondos es mucho más de lo que parece. ¡Descubre 5 maneras creativas de usarla!",
+        "text" : "La herramienta Fondos es mucho más de lo que parece. ¡Descubre 5 maneras creativas de usarla!"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/8264a6e78496c9f68120c979379e1fa2.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 72,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/como-mejorar-la-comunicacion-de-tu-empresa-con-airbrush-y-airbrush-video/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000385",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1658707200,
+          0
+        ],
+        "timezone" : {
+          "id" : "user_timezone",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "Mejora tu presencia en redes sociales con AirBrush Video",
+        "text" : "Mejora tu presencia en redes sociales con AirBrush Video"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/917553042a760dce55e16f85694badd2.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 63,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/utiliza-borrador-como-un-verdadero-experto/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000307",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1653519600,
+          0
+        ],
+        "timezone" : {
+          "id" : "",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "¡Utiliza Borrador como un verdadero experto!",
+        "text" : "¡Utiliza Borrador como un verdadero experto!"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/6e0763e371eba787ef38638259551375.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 46,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/looks-de-maquillaje-y2k/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000260",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1651100400,
+          0
+        ],
+        "timezone" : {
+          "id" : "",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "¡Descubre cómo crear un Maquillaje perfecto Y2K con AirBrush!",
+        "text" : "¡Descubre cómo crear un Maquillaje perfecto Y2K con AirBrush!"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/74286d3acda5aa2f7c397e32fe767ab3.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 33,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/como-editar-tus-fotos-fashionistas/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000191",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1650236400,
+          0
+        ],
+        "timezone" : {
+          "id" : "",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "¡Conoce los tips para editar tus fotos fashionistas!",
+        "text" : "¡Conoce los tips para editar tus fotos fashionistas!"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/442d4fea208783e929b4c10f82b62895.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 27,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/lleva-al-siguiente-nivel-tus-fotos-foodie-con-airbrush-pro/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000108",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1647820800,
+          0
+        ],
+        "timezone" : {
+          "id" : "",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "Lleva al siguiente nivel tus fotos foodie con AirBrush PRO",
+        "text" : "Lleva al siguiente nivel tus fotos foodie con AirBrush PRO"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/3e7b20465d74e62111f13a8472a4a53e.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 24,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://appairbrush.com/es/como-editar-tu-primera-foto-si-eres-emprendedor/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000072",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1645574400,
+          0
+        ],
+        "timezone" : {
+          "id" : "",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "Crea contenido de marca atractivo con estas herramientas",
+        "text" : "Crea contenido de marca atractivo con estas herramientas"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/8a7cdbe5aa540cd3b238ffe2840acda8.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 13,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "https://www.instagram.com/p/CY4PiWEJrzD/",
+      "position" : "HPBL",
+      "rid" : "AB_BAN_00000059",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1643252400,
+          0
+        ],
+        "timezone" : {
+          "id" : "",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "Descubre cómo eliminar personas de tus fotos fácil y rápido",
+        "text" : "Descubre cómo eliminar personas de tus fotos fácil y rápido"
+      },
+      "ended_at" : 0,
+      "media" : {
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/d983d4f83b78fe61e7dddcbb31bb2a70.jpeg"
+      },
+      "sub_status" : 2,
+      "order_no" : 8,
+      "user_status" : 1,
+      "abcode" : 0
+    },
+    {
+      "deeplink" : "airbrush://p_edit/f_makeup_set?content=AB_MKS_00000102&strength=85&photo_type=photo_user",
+      "position" : "HPB",
+      "rid" : "AB_BAN_00000433",
+      "effective_time" : {
+        "status" : 1,
+        "info" : [
+          1665964800,
+          1667264399
+        ],
+        "timezone" : {
+          "id" : "user_timezone",
+          "std_offset" : 0
+        }
+      },
+      "text_layer" : "",
+      "same_using" : {
+        "status" : 2,
+        "using_music_id" : "",
+        "relation_music_list" : [
+
+        ],
+        "desc" : "",
+        "media" : {
+          "type" : "",
+          "url" : ""
+        }
+      },
+      "text_content" : {
+        "title" : "\\",
+        "text" : "\\"
+      },
+      "ended_at" : 1667264399,
+      "media" : {
+        "w" : 1000,
+        "type" : "img",
+        "url" : "https://gcs-airbrush.mr.pixocial.com/021376ba0b0b77389160d093700cf55c.jpeg",
+        "h" : 1000
+      },
+      "sub_status" : 1,
+      "order_no" : 341,
+      "user_status" : 1,
+      "abcode" : 0
+    }
+  ],
+  "message" : "",
+  "update" : "628d4050f40405f2a4e841ab1355045c"
+}
+$done({body: JSON.stringify(obj)});
