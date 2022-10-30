4a9f03faa0cea74ed3a8d1272129faad3b2f82ab
elzari <86085151+elzari@users.noreply.github.com>
2022-10-30 02:25:48 +0100

    Add files via upload

---
+++ OwlFiles.js
@@ -0,0 +1,22 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "accountRegistTime" : 0,
+  "connectedWeixin" : false,
+  "externalUid" : "",
+  "uid" : 11114,
+  "expireAt" : 4097006564000,
+  "memberLevel" : 2,
+  "connectedGoogle" : false,
+  "dispName" : "fedemf38@icloud.com",
+  "errorMessage" : "",
+  "connectedApple" : true,
+  "errorCode" : 0,
+  "withoutPasswd" : true,
+  "email" : "000806.9d1490b5e25140ef97dfdc85489600ec.2336",
+  "succ" : true,
+  "lastPasswordModifiedTime" : 1667086616470
+}
+$done({body: JSON.stringify(obj)});
