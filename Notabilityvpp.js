eb1c1b6a6ee27b336b0e98ad392b03cc516b4986
elzari <86085151+elzari@users.noreply.github.com>
2022-11-01 01:19:17 +0100

    Add files via upload

---
+++ Notabilityvpp.js
@@ -0,0 +1,29 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "data": {
+    "processVPPReceipt": {
+      "__typename": "SubscriptionResult",
+      "error": 0,
+      "subscription": {
+        "__typename": "AppStoreSubscription",
+        "status": "active",
+        "originalPurchaseDate": "2022-10-31T23:35:29.000Z",
+        "originalTransactionId": "300001262346134",
+        "expirationDate": "2099-12-31T23:35:25.000Z",
+        "productId": "com.gingerlabs.Notability.vpp_subscription",
+        "tier": "premium",
+        "refundedDate": null,
+        "refundedReason": null,
+        "isInBillingRetryPeriod": false,
+        "gracePeriodExpiresAt": null,
+        "expirationIntent": null,
+        "overDeviceLimit": false,
+        "user": null
+      }
+    }
+  }
+}
+$done({body: JSON.stringify(obj)});
