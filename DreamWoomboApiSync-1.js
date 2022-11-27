8bd32bb39d75520c8f26f32e1ccb5a1857d32da0
elzari <86085151+elzari@users.noreply.github.com>
2022-11-27 01:09:54 +0100

    Add files via upload

---
+++ DreamWoomboApiSync-1.js
@@ -0,0 +1,12 @@
+var obj = JSON.parse($response.body);
+
+obj = 
+
+{
+  "is_premium": true,
+  "expires_on": "2022-11-29T20:18:34",
+  "discord_id": null,
+  "premium_code": "EQNGX3I4"
+}
+
+$done({body: JSON.stringify(obj)});
